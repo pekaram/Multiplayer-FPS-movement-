af3c72522c29a7da097fcabdf3820ee5b6d72035
pekaram <Petertalaat@hotmail.com>
2021-03-18 20:13:18 +0200

    Added the headless client generator

---
+++ Headless Client Generator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DarkRift.Client;
+using DarkRift;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+using System.ComponentModel;
+
+
+class HeadlessClientGenerator
+{
+    public static List<DarkRiftClient> Clients = new List<DarkRiftClient>();
+
+    private const int NumberOfClientToConnect = 50;
+
+    private const string ServerIP = "41.46.133.246";
+
+    private const int CooldownInMilliseconds = 10;
+
+    static void Main(string[] args)
+    {
+
+        for (var i = 0; i < NumberOfClientToConnect; i++)
+        {
+            Clients.Add(new DarkRiftClient());
+            Clients[i].Connect(IPAddress.Parse(ServerIP), 4296, false);
+            Thread.Sleep(CooldownInMilliseconds);
+        }
+
+    }
+}
